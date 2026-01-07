23f07409e5af7306ff58e78c6f00478f845fd83b
Aakash R <aakashravi.dev@gmail.com>
2026-01-07 22:08:49 +0530

    Time: 7 ms (15.41%) | Memory: 47.2 MB (7.95%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String intToRoman(int num) {
+        HashMap<Integer,String> hm = new HashMap<>();
+        hm.put(1,"I");
+        hm.put(4,"IV");
+        hm.put(5,"V");
+        hm.put(9,"IX");
+        hm.put(10,"X");
+        hm.put(40,"XL");
+        hm.put(50,"L");
+        hm.put(90,"XC");
+        hm.put(100,"C");
+        hm.put(400,"CD");
+        hm.put(500,"D");
+        hm.put(900,"CM");
+        hm.put(1000,"M");
+
+        int [] a = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+
+        String ans = "";
+
+        for(int i = 0;i<a.length;i++){
+            if(num >= a[i]){
+                int temp = num/a[i];
+                num = num%a[i];
+
+                for(int j = 0;j<temp;j++){
+                    ans += hm.get(a[i]);
+                }
+            }
+        }
+        return ans;
+
+    }
+}
