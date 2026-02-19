68629738f145d9c463dba00037fdbb010f1a8112
Aakash R <aakashravi.dev@gmail.com>
2026-02-19 20:06:42 +0530

    Time: 1 ms (84.27%) | Memory: 42.9 MB (54.64%) - LeetSync

---
+++ 394-decode-string/decode-string.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> numStack = new Stack<>();
+        Stack<StringBuilder> strStack = new Stack<>();
+
+        int num = 0;
+        StringBuilder ans = new StringBuilder();
+
+        for(char ch : s.toCharArray()){
+
+            if(Character.isDigit(ch)){
+
+                num = (num*10)+(ch - '0');
+
+            } else if (ch == '[') {
+
+                numStack.push(num);
+                num = 0;
+                strStack.push(ans);
+                ans = new StringBuilder();
+
+            } else if (ch == ']') {
+                String str = ans.toString();
+                ans = new StringBuilder(str.repeat(numStack.pop()));
+                ans = strStack.pop().append(ans);
+            }else{
+                ans.append(ch);
+            }
+
+        }
+        return ans.toString();
+    }
+}
