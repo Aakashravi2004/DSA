d5089a03bf184154304aaa6a41f3207db0feb9af
Aakash R <aakashravi.dev@gmail.com>
2026-03-21 11:26:30 +0530

    Time: 15 ms (54.15%) | Memory: 47.5 MB (88.15%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         
-        HashMap<Integer,Integer> Freqence = new HashMap<>();
+       HashMap<Integer,Integer> Freqence = new HashMap<>();
         for(int num : nums){
             if(Freqence.containsKey(num)){
                 Freqence.put(num,Freqence.getOrDefault(num,0)+1);
@@ -10,22 +10,33 @@ class Solution {
                 Freqence.put(num,1);
             }
         }
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(Freqence.entrySet());
-        list.sort((a, b) -> b.getValue() - a.getValue());
 
-        LinkedHashMap<Integer, Integer> sortedMap = new LinkedHashMap<>();
+       Map<Integer,List<Integer>> reverseFrequence = new HashMap<>();
 
-        for (Map.Entry<Integer, Integer> entry : list) {
-            sortedMap.put(entry.getKey(), entry.getValue());
-        }
-        List<Map.Entry<Integer, Integer>> list1 = new ArrayList<>(sortedMap.entrySet());
+       for(int num : Freqence.keySet()){
+           int count = Freqence.get(num);
+           if(!reverseFrequence.containsKey(count)){
+               reverseFrequence.put(count,new ArrayList<>());
+           }reverseFrequence.get(count).add(num);
 
-        int[] finalArray = new int[k];
+       }
 
-        for (int i = 0; i < k; i++) {
-            finalArray[i] = list1.get(i).getKey();
-        }
-
-       return finalArray;
+       int [] res = new int[k];
+       int index = 0;
+       for (int i = nums.length ; i>=1 ; i--){
+           if(reverseFrequence.containsKey(i)){
+               for (int n : reverseFrequence.get(i)){
+                   if(index < k){
+                       res[index++] = n;
+                   }else{
+                       break;
+                   }
+               }
+           }
+           if(index >= k){
+               break;
+           }
+       }
+    return res;
     }
 }
